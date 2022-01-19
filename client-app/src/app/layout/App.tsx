2dac483bd3fd06ea39e966082d1e54e2c8d3f205
logicalixhumari <i.xhumari@logic.al>
2022-01-19 16:26:51 +0100

    end of section 3

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -43,6 +43,10 @@ function App() {
    setSelectedActivity(activity);
   }
 
+  function handleDeleteActivity(id: string){
+    setActivities([...activities.filter(x=>x.id !== id)])
+  }
+
   return (
     <>
       <NavBar openForm={handleFormOpen}/>
@@ -56,9 +60,10 @@ function App() {
         openForm={handleFormOpen}
         closeForm={handleFormClose}
         createOrEdit={handleCreateOrEditActivity}
+        deleteActivity={handleDeleteActivity}
         />
      </Container>
-       
+        
     </>
   );
 }
