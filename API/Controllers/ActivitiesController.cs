3a39028823ca762074f3cec3efda9cff1ce1f1af
Izauraxh <izaura1997@gmail.com>
2022-04-01 17:49:39 +0200

    First part of section 10

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,9 +1,9 @@
-﻿using Application.Activities;
-using Domain;
-using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Application.Activities;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
@@ -17,10 +17,9 @@ namespace API.Controllers
         /// </summary>
         /// <returns></returns>
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> GetActivities()
+        public async Task<IActionResult> GetActivities()
         {
-           
-            return await Mediator.Send(new List.Query());
+            return HandleResult(await Mediator.Send(new List.Query()));
         }
         /// <summary>
         /// Get an activity with id 
@@ -28,30 +27,43 @@ namespace API.Controllers
         /// <param name="id"></param>
         /// <returns></returns>
         [HttpGet("{id}")]
-        public async Task<ActionResult<Activity>> GetActivity(Guid id)
-        {
-            return await Mediator.Send(new Details.Query { Id = id });
+        public async Task<IActionResult> GetActivity(Guid id)
+        {     
+             return HandleResult(await Mediator.Send(new Details.Query { Id = id }));
         }
-
+        /// <summary>
+        /// Creates a new activity
+        /// </summary>
+        /// <param name="activity"></param>
+        /// <returns></returns>
         [HttpPost]
         public async Task<IActionResult> CreateAcitivity(Activity activity)
         {
-            return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
+            return HandleResult(await Mediator.Send(new Create.Command { Activity = activity }));
         }
-
+        /// <summary>
+        /// Update an activity
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="activity"></param>
+        /// <returns></returns>
         [HttpPut("{id}")]
         public async Task<IActionResult> EditActivity(Guid id, Activity activity)
         {
             activity.Id = id;
-            return Ok(await Mediator.Send(new Edit.Command { Activity = activity }));
+            return HandleResult(await Mediator.Send(new Edit.Command { Activity = activity }));
         }
-
+        /// <summary>
+        /// Delete an activity
+        /// </summary>
+        /// <param name="id">Id of activity</param>
+        /// <returns></returns>
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteActivity(Guid id)
         {
-           
-            return Ok(await Mediator.Send(new Delete.Command { Id = id }));
+
+            return HandleResult(await Mediator.Send(new Delete.Command { Id = id }));
         }
-     
+
     }
 }
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -7,12 +7,15 @@ import { Route, useLocation } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import TestErrors from '../../features/errors/TestError';
+import { ToastContainer } from 'react-toastify';
 
 
 function App() {
   const location = useLocation();
   return (
     <>
+    <ToastContainer position='bottom-right' hideProgressBar/>
      <Route exact path='/' component={HomePage}/>
      <Route 
      path={'/(.+)'}
@@ -23,6 +26,7 @@ function App() {
         <Route exact path='/activities' component={ActivityDashboard}></Route>
         <Route path='/activities/:id' component={ActivityDetails}></Route>
         <Route key={location.key} path={['/createActivity','/manage/:id']} component={ActivityForm}></Route>
+        <Route path='/errors' component={TestErrors}></Route>
       </Container>
        </>
      )}
