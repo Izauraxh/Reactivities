bce506f39ea5a4c0289b922fbb55be12c49e3124
Izauraxh <izaura1997@gmail.com>
2022-04-14 14:35:42 +0200

    end Section 13

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -51,26 +51,28 @@ namespace API.Controllers
         {
             if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email))
             {
-                return BadRequest("Email taken");
+                ModelState.AddModelError("email", "Email taken!");
+                return ValidationProblem();
             }
             if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.Username))
-            {
-                return BadRequest("Username taken");
+            {               
+                ModelState.AddModelError("username", "Username taken!");
+                return ValidationProblem();
             }
 
             var user = new AppUser
             {
                 DisplayName = registerDto.DisplayName,
                 Email = registerDto.Email,
-                UserName = registerDto.Username
+                UserName = registerDto.Username              
 
             };
-            var result = await _userManager.CreateAsync(user);
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
             if (result.Succeeded)
             {
                 CreateObjectUser(user);
             }
-            return BadRequest();
+            return BadRequest("Problem registering user");
         }
 
         [Authorize]
