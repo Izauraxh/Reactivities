4610642ccb1d2950a92fa8777b19358ee8c16d72
Izauraxh <izaura1997@gmail.com>
2022-04-26 17:59:11 +0200

    mainpic

--- client-app/src/features/profiles/ProfilePhotos.tsx
+++ client-app/src/features/profiles/ProfilePhotos.tsx
@@ -1,44 +1,53 @@
 import { observer } from "mobx-react-lite";
 import React, { useState } from "react";
-import { Card,Image, Header, Tab, Grid, Button } from "semantic-ui-react";
+import { Card, Image, Header, Tab, Grid, Button } from "semantic-ui-react";
 import PhotoUploadWidget from "../../app/common/imageUpload/PhotoUploadWidget";
 import { Profile } from "../../app/models/profile";
 import ProfileStore from "../../app/stores/profileStore";
 import { useStore } from "../../app/stores/store";
 interface Props {
-    profile : Profile;
+    profile: Profile;
 }
-export default observer( function ProfilePhotos({profile}:Props){
-    const {profileStore:{isCurrentUser}}= useStore();
+export default observer(function ProfilePhotos({ profile }: Props) {
+    const { profileStore: { isCurrentUser, uploadPhoto, uploading ,loading, setMainPhoto} } = useStore();
     const [addPhotoMode, setAddPhotoMode] = useState(false);
-  return (
-    <Tab.Pane>
-        <Grid>
-            <Grid.Column width={16}>
-            <Header floated='left' icon='image' content='photos'/>
-            {isCurrentUser && (
-                <Button floated="right" basic 
-                content={addPhotoMode? 'Cancel' : 'Add Photo'}
-                onClick={()=> setAddPhotoMode(!addPhotoMode)}
-                />
-            )}
-            </Grid.Column>
-            <Grid.Column width={16}>
-             {addPhotoMode ? (<PhotoUploadWidget/>) :
-             
-             (  <Card.Group itemsPerRow={5}>
-            {profile.photos?.map(photo =>(
-                <Card key={photo.id}>
-                <Image src={photo.url}/>
-            </Card> 
-            ))}       
-            
-        </Card.Group>)}
-            </Grid.Column>
-   
-        </Grid>
-       
-      
-    </Tab.Pane>
-  )
+    function handlePhotoUpload(file: Blob) {
+        uploadPhoto(file).then(() => setAddPhotoMode(false))
+    }
+    return (
+        <Tab.Pane>
+            <Grid>
+                <Grid.Column width={16}>
+                    <Header floated='left' icon='image' content='photos' />
+                    {isCurrentUser && (
+                        <Button floated="right" basic
+                            content={addPhotoMode ? 'Cancel' : 'Add Photo'}
+                            onClick={() => setAddPhotoMode(!addPhotoMode)}
+                        />
+                    )}
+                </Grid.Column>
+                <Grid.Column width={16}>
+                    {addPhotoMode ? (<PhotoUploadWidget uploadPhoto={handlePhotoUpload} loading={uploading} />) :
+
+                        (<Card.Group itemsPerRow={5}>
+                            {profile.photos?.map(photo => (
+                                <Card key={photo.id}>
+                                    <Image src={photo.url} />
+                                    {isCurrentUser&& (
+                                        <Button.Group fluid widths={2}>
+                                            <Button/>
+
+                                        </Button.Group>
+                                    )}
+                                </Card>
+                            ))}
+
+                        </Card.Group>)}
+                </Grid.Column>
+
+            </Grid>
+
+
+        </Tab.Pane>
+    )
 })
